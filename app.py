a0c571a573396dde08345b7153377dded61ebed0
Eert <eert.hoogerwerf@gmail.com>
2021-07-24 17:30:41 +0200

    Added Dockerfile

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def disconnected():
 
 @socketio.on('message')
 def handle_message(data):
-    print('Received message: ' + data)
+    print(f'Received message: {data}')
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
